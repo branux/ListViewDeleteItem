1d81e4f8fd215d7fbcd2ddf9476461f20a38026f
Pedro Jesus <pedro.pj.souza@hotmail.com>
2017-07-19 20:51:09 -0300

    Principal
    projeto full

---
+++ DeleteList/DeleteList/Model/Lista.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.ObjectModel;
+
+namespace DeleteList.Model
+{
+    public class Lista
+    {
+        public string Nome { get; set; }
+        public string Idade { get; set; }
+
+        public Lista(string nome, string idade)
+        {
+            Nome = nome;
+            Idade = idade;
+        }
+    }
+
+    
+    
+}
---
+++ DeleteList/DeleteList/ViewModel/MainViewModel.cs
@@ -0,0 +1,42 @@
+﻿using DeleteList.Model;
+using System.Collections.ObjectModel;
+using Xamarin.Forms;
+
+namespace DeleteList.ViewModel
+{
+    public class MainViewModel : BaseViewModel
+    {
+    
+
+
+        public Command DeletarCommand { get; }
+        public ObservableCollection<Lista> Lista { get; }
+        public Lista ItemSelecionado { get; set; }
+
+        public MainViewModel()
+        {
+            Lista = new ObservableCollection<Lista>
+            {
+                new Lista("Pedro", "23"),
+                new Lista("Clara", "24"),
+                new Lista("Gustavo", "25"),
+                new Lista("Luciano", "26")
+            };
+
+
+            
+          
+                          
+            DeletarCommand = new Command(ExecuteDeletarCommand);
+        }      
+
+        void ExecuteDeletarCommand()
+        {
+            if(ItemSelecionado != null)
+            {
+                Lista.Remove(ItemSelecionado);
+                
+            }
+        }
+    }
+}
